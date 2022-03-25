8c70d561724019a48077cc09cf8d5b363f4c4954
wudada <2567199685@qq.com>
2022-03-25 17:00:25 +0800

    插入 svg 图片

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,66 @@
-import React from 'react';
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import styled from "styled-components";
+import Nav from "components/Nav";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+`;
+
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/tags" />
+            </Route>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav />
+      </Wrapper>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return <div>页面不存在，你丫输错地址了吧！</div>;
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页</h2>;
 }
 
 export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import x from "components/icons/money.svg";
+
+const NavWrapper = styled.nav`
+  line-height: 24px;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  > ul {
+    display: flex;
+    > li {
+      width: 33.33%;
+      text-align: center;
+    }
+  }
+`;
+
+const Nav = () => {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <img src={x} alt="" />;<Link to="/tags">标签页</Link>
+        </li>
+        <li>
+          <Link to="/money">记账页</Link>
+        </li>
+        <li>
+          <Link to="/statistics">统计页</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  );
+};
+export default Nav;
